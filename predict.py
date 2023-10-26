a940240098ce72c98ba356af0f345e0eb7f2672d
pkseniya <petrushina.ke@phystech.edu>
2023-10-26 23:13:08 +0300

    change prediction

--- predict.py
+++ predict.py
@@ -1,25 +1,44 @@
-import argparse
+import requests
 
 from transformers import ViTImageProcessor, ViTModel, ViTForImageClassification
 from PIL import Image
 
-def configure_arg_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("image_path", type=str, help="Path for image to classify")
-    return parser
-
-def main(image_path: str):
+def main():
     labels = ["cat", "dog"]
-    image = Image.open(image_path)
 
-    feature_extractor = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224-in21k')
-    model =  ViTForImageClassification.from_pretrained('akahana/vit-base-cats-vs-dogs')
-    inputs = feature_extractor(images=image, return_tensors="pt")
+    while True:
+        try:
+            path = input("Enter image path/url: ")
 
-    outputs = model(**inputs)
-    print(f"This is a {labels[outputs.logits.argmax()]}!")
+            if path.startswith("http"):
+                try:
+                    image_path = requests.get(path, stream=True).raw
+                except requests.exceptions.MissingSchema:
+                    print("Invalid URL")
+                    continue
+            else:
+                image_path = path
+
+            try:
+                image = Image.open(image_path)
+            except FileNotFoundError:
+                print(f"Can't open image on the {path}")
+                continue
+            except PIL.UnidentifiedImageError:
+                print(f"URL f{path} doesn't lead to image")
+                continue
+
+            feature_extractor = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224-in21k')
+            model =  ViTForImageClassification.from_pretrained('akahana/vit-base-cats-vs-dogs')
+            inputs = feature_extractor(images=image, return_tensors="pt")
+
+            outputs = model(**inputs)
+            print(f"This is a {labels[outputs.logits.argmax()]}!")
+
+        except KeyboardInterrupt:
+            print("\nSystem shutdown")
+            break
 
 if __name__ == "__main__":
-    _args = configure_arg_parser().parse_args()
-    main(**vars(_args))
+    main()
 
